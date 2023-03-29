826dbf5e553dfcca4d395542477882a831922a6a
AugusMeow <bramlye@outlook.com>
2023-03-29 17:32:08 +0800

    chore: new dockerfile base dm8

---
+++ conf.modify.dockerfile
@@ -0,0 +1,31 @@
+FROM dm8_single:v8.1.2.128_ent_x86_64_ctm_pack4 AS modfiy_dm8
+
+COPY sources.list /etc/apt/
+
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    cmake \
+    git \
+    vim \
+    gcc \
+    g++ \
+    gdb \
+    nano
+
+RUN apt-get install -y \
+    unixodbc unixodbc-dev
+
+FROM modfiy_dm8 AS dm_cpp_demo_runtime
+
+# check odbc install
+RUN odbcinst -j
+
+COPY etc/odbc.ini /etc/odbc.ini
+COPY etc/odbcinst.ini /etc/odbcinst.ini
+
+WORKDIR /app
+COPY . /app
+
+# RUN g++ -o main main.cpp
+
+# CMD ["./main"]
--- docker.sh
+++ docker.sh
@@ -1,20 +1,67 @@
 #!/bin/bash
 
-if [ -z $1 ]; then
-    echo "no option, exit" && echo " "
-    echo "Usage: $0 [OPTION]"
-    echo "OPTION: run [r] | build [b]"
+PARA_NUM=$#
+
+# tag 1: -b or -r
+TAG_1=$1
+# tag 2: -name
+TAG_2=$3
+
+if [[ $PARA_NUM -ne 2 && $PARA_NUM -ne 4 ]];then
+    echo "Insufficient parameters, exit" && echo " "
+    echo "Usage: $0 [-b DOCKER NAME]"
+    echo "       $0 [-r DOCKER NAME] [-name RUN NAME]"
     exit 1
 fi
 
 CUSTOM_BRIDGE_NAME=dm_network
 
-DOCKER_NAME=dm_demo
-RUN_NAME=dm_demo
+docker_name=dm_demo
+run_name=dm_demo
+
+docker_name=$2
+run_name=$4
 
 . ./docker_kill.sh
-rm_docker $RUN_NAME
 
+
+check_para(){
+    [[ $PARA_NUM -ne $1 ]] && echo "check para err, para $1" && exit 1
+    echo "check para num passed"
+
+    case $1 in
+    4)
+        [[ $TAG_1 != "-r" ]] && echo "tag -r err" && exit 1
+        [[ $TAG_2 != "-name" ]] && echo "tag -name err" && exit 1
+        ;;
+    2)
+        [[ $TAG_1 != "-b" ]] && echo "tag -b err" && exit 1
+        ;;
+    *)
+        ;;
+    esac
+}
+
+
+docker_build() {
+    if [ -z $1 ]; then
+        echo "no option, exit"
+        exit 1
+    fi
+
+    echo "build ${1}"
+    echo " "    
+
+    case $1 in
+    demo0)
+        docker build -t ${1} .
+        ;;
+    demo1)
+        docker build -t ${1} -f conf.modify.dockerfile .
+        ;;
+    *);;
+    esac
+}
 
 docker_run() {
     if [ -z $1 ]; then
@@ -34,7 +81,6 @@ docker_run() {
         rm_docker dm8_01
         docker run -d \
             -p 5236:5236 \
-            --restart=always \
             --name dm8_01 \
             --network ${CUSTOM_BRIDGE_NAME} \
             --ip 172.20.0.100 \
@@ -45,13 +91,17 @@ docker_run() {
             -v /data/dm8_01:/opt/dmdbms/data \
             dm8_single:v8.1.2.128_ent_x86_64_ctm_pack4
         ;;
-    demo)
+    demo0)
         docker run -it \
-            --name ${RUN_NAME} \
+            --name ${run_name} \
             --network ${CUSTOM_BRIDGE_NAME} \
             --ip 172.20.0.6 \
-            -v /opt/dmdbms/bin:/opt/dmdbms/bin \
-            ${DOCKER_NAME}:latest
+            ${docker_name}:latest
+        ;;
+    demo1)
+        docker run -it \
+            --name ${run_name} \
+            ${docker_name}:latest
         ;;
     *)
         ;;
@@ -59,26 +109,16 @@ docker_run() {
 }
 
 
-
 case $1 in
-run)
+-r)
+    check_para 4
+    rm_docker $run_name
     docker_run $2
     exit 0
     ;;
-r)
-    docker_run $2
-    exit 0
-    ;;
-build)
-    echo "build ${DOCKER_NAME}"
-    echo " "
-    docker build -t ${DOCKER_NAME} .
-    exit 0
-    ;;
-b)
-    echo "build ${DOCKER_NAME}"
-    echo " "
-    docker build -t ${DOCKER_NAME} .
+-b)
+    check_para 2
+    docker_build $2
     exit 0
     ;;
 *)
