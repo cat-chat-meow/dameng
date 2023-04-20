456e4688895d181ec7e7894d23ba7b0a2962dd7d
AugusMeow <bramlye@outlook.com>
2023-04-20 16:58:20 +0800

    feat: 防止误删 docker

--- docker_kill.sh
+++ docker_kill.sh
@@ -8,6 +8,7 @@ rm_docker() {
     echo -e "# kill rm exist\n"
     rm_id=$(docker ps -a | grep "$1" | cut -d ' ' -f 1)
     rm_arr=(${rm_id// /})
+    echo "need kill dockers are: ${rm_arr[@]}"
     for id in ${rm_arr[@]}
     do
         # echo ">>> rm id $id"
--- run.sh
+++ run.sh
@@ -32,5 +32,27 @@ echo ""
 [ -n "$2" ] && echo "need build" && \
     ./docker.sh -b demo$1 && \
     ./docker.sh -r demo$1 -name $run_name
-    
-docker exec -it $run_name bash
+
+
+run_id=$(docker ps -a | grep "$run_name" | cut -d ' ' -f 1)
+run_id_arr=(${run_id// /})
+# 这个数组长度必为1 如果不为1 要看看 运行名字是否重叠 或者有其他问题
+[ ${#run_id_arr[@]} -ne 1 ] && echo "!!! check your run name,  !!!"
+if [ ${#run_id_arr[@]} -eq 0 ]; then
+    # 没有在 run 的 立即 run
+    ./docker.sh -r demo$1 -name $run_name    
+    docker exec -it $run_name bash
+    echo ""
+else
+    # 有的 过滤一下 看是 Up 还是 Exited
+    run_info=$(docker ps -a | grep "$run_name")
+    if [[ "$run_info" != *"Up"* ]]; then
+        echo "is not running"
+        ./docker.sh -r demo$1 -name $run_name    
+        docker exec -it $run_name bash
+    else
+        echo "is running"
+        docker exec -it $run_name bash
+    fi
+    echo ""
+fi
